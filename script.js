1d37435325f8c519c2b364feff956a86f56b71e1
Mohammed_Thanis <thanis7168t@gmail.com>
2024-01-14 13:15:54 +0530

    almost

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-
-document.getElementById('appoinment').addEventListener('click', function () {
-    document.getElementById('reg-form-container').style.display = 'flex';
+document.getElementById("appoinment").addEventListener("click", function () {
+  document.getElementById("reg-form-container").style.display = "flex";
 });
 
-document.getElementById('close-reg-form-btn').addEventListener('click', function () {
-    document.getElementById('reg-form-container').style.display = 'none';
-});
+document
+  .getElementById("close-reg-form-btn")
+  .addEventListener("click", function () {
+    document.getElementById("reg-form-container").style.display = "none";
+  });
 
-document.getElementById('reg-form').addEventListener('submit', function (event) {
+document
+  .getElementById("reg-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     // Handle form submission here
-});
+  });
 
-function openMenu(){
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('open');
+function openMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("open");
 }
-
