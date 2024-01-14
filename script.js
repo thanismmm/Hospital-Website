44783a7b9186b7de4d2c11eff7f7c7a03add3e7c
Mohammed_Thanis <thanis7168t@gmail.com>
2024-01-14 15:02:32 +0530

    focus button

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+
+// Cache the list, and the items
+const list = document.querySelector('nav ul');
+const links = list.querySelectorAll('a');
+
+
+// Add one listener to the list element
+list.addEventListener('click', handleClick);
+
+// If the clicked element is a link remove all
+// the active classes from the other links, and then
+// add the active class to the link that was clicked on
+function handleClick(e) {
+  if (e.target.matches('a')) {
+    links.forEach(link => link.classList.remove('active'));
+    e.target.classList.add('active');
+  }
+}
+
+
+
+
+
+
 document.getElementById("appoinment").addEventListener("click", function () {
   document.getElementById("reg-form-container").style.display = "flex";
 });
 
-document
-  .getElementById("close-reg-form-btn")
-  .addEventListener("click", function () {
-    document.getElementById("reg-form-container").style.display = "none";
+document.getElementById("close-reg-form-btn").addEventListener("click", function () {
+  document.getElementById("reg-form-container").style.display = "none";
   });
 
-document
-  .getElementById("reg-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById("reg-form").addEventListener("submit", function (event) {
+  event.preventDefault();
     // Handle form submission here
   });
 
